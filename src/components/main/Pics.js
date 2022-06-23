8e574efdf4ec6075f8515a4f2d7f841610e5be34
heyonjin <gdalove@naver.com>
2022-06-23 20:15:58 +0900

    메인 반응형 추가

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -8,12 +8,10 @@ function Pics({ Scrolled, start, base }) {
 	let position = 0;
 	if (start) position = Scrolled - start - base;
 
-	const [Index, setIndex] = useState(0);
-	
 	const video = useRef(null);
 
 	const onMouseEnter = (e) => {
-		console.log(1);
+		video.current.style.diplay ='none';
 		
 	};
 	const onMouseLeave = (e) => {
@@ -24,8 +22,6 @@ function Pics({ Scrolled, start, base }) {
 		<>		
 		<section id='pic' className='myScroll'>
 			<h2>Pics</h2>
-			{/* <p style={{ left: 100 + position }}>FLICKR</p>
-			<h3 style={{ left: 100 + position / 2 }}>FLICKR</h3> */}
 			<div className='box'>
 				{pics.map((item, idx) => {
 					if (idx < 8) {
@@ -37,7 +33,9 @@ function Pics({ Scrolled, start, base }) {
 									<h3>{item.title}</h3>
 									<img src={`${path}/img/${item.pic}`} 
 									alt={item.title}/>
-									<video muted src={`${path}/img/${item.video}`} className='video' ref={video}></video>
+									<video muted src={`${path}/img/${item.video}`} className='video' ref={video} >
+									
+									</video>
 							</div>
 						);
 					}
